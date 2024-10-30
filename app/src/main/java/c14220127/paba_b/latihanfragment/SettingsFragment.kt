2f19828f4e70941b252685c9c2f56f49f54a6d59
Cogzzz <oentorosteven@gmail.com>
2024-10-30 16:23:27 +0700

    modif

--- app/src/main/java/c14220127/paba_b/latihanfragment/SettingsFragment.kt
+++ app/src/main/java/c14220127/paba_b/latihanfragment/SettingsFragment.kt
@@ -9,7 +9,6 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.RadioGroup
-import android.widget.Toast
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
